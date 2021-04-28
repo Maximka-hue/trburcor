7c29561e8bcfb97f2328fc63a3830e4dbe6d5d52
Maximka-hue <53125181+Maximka-hue@users.noreply.github.com>
2021-04-28 13:23:41 +0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -3,12 +3,20 @@ extern crate libc;
 extern crate cc;
 use libc::{c_double, c_int};
 
-#[link(name = "smooth")]
+//#[link(name = "smooth_correct_utf")]
 extern "C" {
-    fn Smooth_Array_Zhmakin_Fursenko(Fs:*mut c_double, Nmax: c_int, smooth_intensity: c_double, Fi: *mut c_double, Ftd: *mut c_double) ->  c_int;
+    fn smooth_arr_zm_fur(Fs:*mut c_double, Nmax: c_int, smooth_intensity: c_double, Fi: *mut c_double, Ftd: *mut c_double) ->  c_int;
 }
 
 fn main() {
+    let env_vars = env::vars();
+    for(key, value) in env_vars.into_iter() {
+        println!("{} = {:?}", key, value);
+    }
+    cc::Build::new()
+        .file("src/smooth_correct_utf.c")
+        .compile("smooth_correct_utf");
+    println!("Hello, world!, {}",  env::var("OUT_DIR").unwrap());
     let all_steps: i32 = 1000;
     let width = all_steps as usize;
     let mut exact_solvec = vec![vec![0_f32; (all_steps + 2) as usize], vec![0_f32;(all_steps + 2) as usize], vec![0_f32;(all_steps + 2) as usize]];
@@ -19,6 +27,6 @@ fn main() {
     let mut first_correction = vec![0_f32; width];
     let mut second_correction = vec![0_f32; width];
     let smooth_intensity = 0.5;
-    unsafe{  Smooth_Array_Zhmakin_Fursenko(inner_vector.as_mut_ptr() as *mut f64, all_steps as i32, smooth_intensity,
-        first_correction.as_mut_ptr() as *mut f64, second_correction.as_mut_ptr() as *mut f64);}
+    //unsafe{  smooth_arr_zm_fur(inner_vector.as_mut_ptr() as *mut f64, all_steps as i32, smooth_intensity,
+    //    first_correction.as_mut_ptr() as *mut f64, second_correction.as_mut_ptr() as *mut f64);}
 }
